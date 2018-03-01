bbff4d7e4e3e34711aa7d80e90590f43c528fc1f
Mitzicom Admin <mitzicom-admin@example.com>
2018-03-01 17:21:34 -0500

    Updates to nfs config

--- 4.prepare-nfs.sh
+++ 4.prepare-nfs.sh
@@ -1,7 +1,8 @@
-ansible nfs -m shell -a "mkdir -p /exports/user-vols/pv{001..200};
-  for pvnum in {001..200} ; do \
-  echo /exports/user-vols/pv${pvnum} *(rw,root_squash) >> /etc/exports.d/openshift-uservols.exports; \
+ansible nfs -m shell -a "mkdir -p /exports/user-vols/pv{001..200};"
+
+for pvnum in {001..200} do \
+  ansible nfs -m shell -a "echo /exports/user-vols/pv${pvnum} *(rw,root_squash) >> /etc/exports.d/openshift-uservols.exports; \
   chown -R nfsnobody.nfsnobody  /exports/user-vols; \
-  chmod -R 777 /exports/user-vols; \
-  done;"
+  chmod -R 777 /exports/user-vols;"
+done;
 ansible nfs -m shell -a "systemctl restart nfs-server"
